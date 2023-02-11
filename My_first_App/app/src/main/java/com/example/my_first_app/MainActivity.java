ff579cd07184eb33a5bd99dea1a7691fa7647dda
chetan kumar <manvirfeb97@gmail.com>
2023-02-11 02:01:47 -0500

    creating first commit with a simple application that shows the user enter name with hello

---
+++ My_first_App/app/src/main/java/com/example/my_first_app/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.my_first_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private Button ShowText;
+    private TextView nameText;
+    private EditText enterName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ShowText= findViewById(R.id.button);
+        nameText=findViewById(R.id.textView);
+        enterName=findViewById(R.id.editTextName);
+
+
+        ShowText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name= enterName.getText().toString();
+                nameText.setText("Hello " +name);
+
+            }
+        });
+        //nameText.setText("hello from java");
+    }
+}
